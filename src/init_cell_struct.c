41aa206649e327eb06155ad65b2c3f3c1fd2ecc6
Raphael Goncalves <Raphal Goncalves>
2024-03-19 19:12:39 +0100

    fix: compile warnings

--- src/init_cell_struct.c
+++ src/init_cell_struct.c
@@ -7,17 +7,18 @@
 
 #include "../include/amazed.h"
 
-static int init_bool(cell_t *cell, size_t nb_robots)
+static int init_bool(cell_t *cell, size_t nb_robots, u_char state)
 {
     cell->robots = malloc(sizeof(_Bool) * nb_robots);
     if (cell->robots == NULL)
         return 84;
     if (state == START)
-        for (int i = 0; i < nb_robots; ++i)
+        for (int i = 0; (size_t)i < nb_robots; ++i)
             cell->robots[i] = 1;
     else
-        for (int i = 0; i < nb_robots; ++i)
+        for (int i = 0; (size_t)i < nb_robots; ++i)
             cell->robots[i] = 0;
+    return 0;
 }
 
 cell_t *init_cell(u_char state, char *name, size_t nb_robots)
@@ -32,8 +33,8 @@ cell_t *init_cell(u_char state, char *name, size_t nb_robots)
     if (my_strlen(name) > MAX_NAME_LENGTH)
         return NULL;
     my_strcpy(cell->name, name);
-    if (init_bool(cell, nb_robots) == 84)
-        return 84;
+    if (init_bool(cell, nb_robots, state) == 84)
+        return NULL;
     cell->robots_nb = nb_robots;
     return cell;
 }
